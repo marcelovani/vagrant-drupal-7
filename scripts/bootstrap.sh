0d3006a9a7583eefa0358384a99b9444575d682a
Marcelo <marcellovani@yahoo.co.uk>
2013-09-30 23:10:37 +0100

    Update bootstrap.sh
    changed url of git repo

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -8,7 +8,7 @@ echo ""
 echo "Bootstrap is installing drupal 7 on virtual box with vagrant..."
 echo "Cloning repository https://github.com/delphian/vagrant-drupal-7..."
 echo ""
-git clone --recursive https://github.com/delphian/vagrant-drupal-7.git
+git clone --recursive https://github.com/marcelovani/vagrant-drupal-7.git
 cd vagrant-drupal-7/scripts
 ./install.sh
 echo "Bootstrap has finished installation."
